5d14c01734d797ab83dc9bebed16e290c477612b
sandergl <sandergl@rose-hulman.edu>
2018-09-06 16:58:45 -0400

    me

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Garrett Sanders.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -95,8 +95,16 @@ def draw_parallel_lines(n, point, length, window):
       :type length: int
       :type window: rg.RoseWindow
     """
+    point1 = rg.Point((point.x + length), point.y)
+
+    for k in range(n):
+        line = rg.Line(point, point1)
+        line.attach_to(window)
+        point = rg.Point(point.x, (point.y + 30))
+        point1 = rg.Point(point1.x, point.y)
+    window.render()
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -160,6 +168,14 @@ def draw_lines(n, point, window):
       :type point: rg.Point
       :type window: rg.RoseWindow
     """
+    point1 = rg.Point((point.x + 100), point.y + 100)
+    z = 200/(n-1)
+    for k in range(n):
+        line = rg.Line(point, point1)
+        line.attach_to(window)
+        point1 = rg.Point(point1.x, (point1.y - z))
+
+    window.render()
     # ------------------------------------------------------------------
     # TODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
